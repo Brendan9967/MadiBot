02ac26f63838a4f6a194d0892629bd5b2dcb3b1f
Madison Tries <ryansweigert@gmail.com>
2013-03-30 08:03:27 -0500

    Basic functioning bot, .gitingore, package.json

---
+++ bot.js
@@ -0,0 +1,24 @@
+var ircLib = require('irc');
+var fs = require('fs');
+
+var client = new ircLib.Client('irc.freenode.net','' , {
+	channels: [''],
+});
+
+var commandChar = "*"
+
+client.addListener('message', function (from, to, message) {
+	console.log(to + ' => ' + from + ': ' + message);
+	if (message.indexOf(commandChar + "meow") > -1){
+		client.say(to,"Meow");
+	} else if (message.indexOf(commandChar + "time") > -1){
+		var nownow = new Date();
+		client.say(to,nownow);
+	} else if (message.indexOf(commandChar + "help") > -1){
+		client.notice(from,"meow, time, help");
+	}
+});
+
+addListener('error', function(message) {
+	conclient.sole.log('error: ', message);
+});
